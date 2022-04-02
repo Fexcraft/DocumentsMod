3a3bf9c679f68835675c8be1ca864eeaeb381a1b
Ferdinand Calo <fercalo96@yahoo.de>
2022-04-03 00:53:55 +0200

    Update DocItemHandler.java

--- src/main/java/net/fexcraft/mod/doc/cap/DocItemHandler.java
+++ src/main/java/net/fexcraft/mod/doc/cap/DocItemHandler.java
@@ -3,9 +3,14 @@ package net.fexcraft.mod.doc.cap;
 import static net.fexcraft.mod.doc.cap.DocItemCapability.CAPABILITY;
 import static net.fexcraft.mod.doc.data.DocumentItem.NBTKEY;
 
+import java.util.UUID;
+
+import com.mojang.authlib.GameProfile;
+
+import net.fexcraft.lib.common.math.Time;
+import net.fexcraft.lib.mc.utils.Static;
 import net.fexcraft.mod.doc.DocRegistry;
 import net.fexcraft.mod.doc.data.Document;
-import net.fexcraft.mod.doc.data.DocumentItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTBase;
@@ -69,8 +74,8 @@ public class DocItemHandler implements ICapabilitySerializable<NBTBase>{
 	public static class Implementation implements DocItemCapability {
 		
 		private ItemStack stack;
+		private NBTTagCompound copy;
 		private Document doc;
-		private boolean issued;
 
 		public void setup(ItemStack stack){
 			this.stack = stack;
@@ -81,124 +86,47 @@ public class DocItemHandler implements ICapabilitySerializable<NBTBase>{
 
 		@Override
 		public void parse(NBTTagCompound compound){
+			if(!compound.hasKey(NBTKEY)) return;
 			doc = DocRegistry.DOCS.get(compound.getString(NBTKEY));
-			issued = compound.getBoolean("issued");
+			copy = compound.copy();
 		}
 
 		@Override
 		public Document getDocument(){
 			if(doc == null){
-				if(stack.getTagCompound().hasKey(DocumentItem.NBTKEY)){
-					parse(stack.getTagCompound());
-				}
+				parse(stack.getTagCompound());
+			}
+			if(!stack.getTagCompound().equals(copy)){
+				parse(stack.getTagCompound());
 			}
 			return doc;
 		}
 
 		@Override
 		public boolean isIssued(){
-			return issued;
+			return stack.getTagCompound().hasKey("document:issued");
 		}
 
 		@Override
 		public String getValue(String key){
-			if(!stack.getTagCompound().hasKey("value:" + key)) return null;
-			return stack.getTagCompound().getString("value:" + key);
+			if(!stack.getTagCompound().hasKey("document:" + key)) return null;
+			return stack.getTagCompound().getString("document:" + key);
 		}
 
 		@Override
 		public void setValue(String key, String val){
-			stack.getTagCompound().setString("value:" + key, val);
+			stack.getTagCompound().setString("document:" + key, val);
 		}
 
 		@Override
 		public void issueBy(EntityPlayer player, boolean client){
-			// TODO Auto-generated method stub
-			
-		}
-
-		/*@Override
-		public NBTBase write(){
-			NBTTagCompound compound = new NBTTagCompound();
-			if(doc == null) return compound;
-			compound.setString("type", doc.id);
-			NBTTagList list = new NBTTagList();
-			values.entrySet().forEach(entry -> {
-				NBTTagCompound com = new NBTTagCompound();
-				com.setString("key", entry.getKey());
-				com.setString("val", entry.getValue());
-				list.appendTag(com);
-			});
-			compound.setTag("values", list);
-			if(issuer != null){
-				compound.setLong("issued", issued);
-				compound.setString("issuer", issuer.toString());
-			}
-			return compound;
-		}
-
-		@Override
-		public void read(NBTTagCompound compound){
-			if(compound == null) return;
-			if(doc == null && compound.hasKey("type")){
-				doc = DocRegistry.DOCS.get(compound.getString("type"));
-			}
-			if(doc == null) return;
-			save = compound;
-			if(compound.hasKey("values")){
-				values.clear();
-				NBTTagList list = (NBTTagList)compound.getTag("values");
-				for(NBTBase base : list){
-					NBTTagCompound com = (NBTTagCompound)base;
-					values.put(com.getString("key"), com.getString("val"));
-				}
-			}
-			if(compound.hasKey("issuer")){
-				issued = compound.getLong("issued");
-				issuer = UUID.fromString(compound.getString("issuer"));
-			}
-		}
-
-		@Override
-		public Document getDocument(){
-			return doc;
-		}
-
-		@Override
-		public Map<String, String> getValues(){
-			return values;
-		}
-
-		@Override
-		public void reload(String type){
-			this.doc = DocRegistry.DOCS.get(type);
-			this.read(save);
-		}
-
-		@Override
-		public UUID getIssuer(){
-			return issuer;
-		}
-
-		@Override
-		public boolean isIssued(){
-			return issuer != null;
-		}
-
-		@Override
-		public boolean isBlank(){
-			return issuer == null;
-		}
-
-		@Override
-		public void issueBy(EntityPlayer player, boolean client){
-			values.put("issuer", (issuer = player.getGameProfile().getId()).toString());
-			values.put("issued", (issued = Time.getDate()) + "");
-			values.put("issuer_name", player.getGameProfile().getName());
+			setValue("issuer", player.getGameProfile().getId().toString());
+			setValue("issued", Time.getDate() + "");
+			setValue("issuer_name", player.getGameProfile().getName());
 			if(client) return;
-			GameProfile gp = Static.getServer().getPlayerProfileCache().getProfileByUUID(UUID.fromString(values.get("uuid")));
-			values.put("player_name", gp.getName());
-		}*/
+			GameProfile gp = Static.getServer().getPlayerProfileCache().getProfileByUUID(UUID.fromString(getValue("uuid")));
+			setValue("player_name", gp.getName());
+		}
 		
 	}
 
