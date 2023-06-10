1b86d68f5684a074de149134f428b14436d9c8ef
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-11 00:05:24 +0200

    Update DocMod.java

--- src/main/java/net/fexcraft/mod/doc/DocMod.java
+++ src/main/java/net/fexcraft/mod/doc/DocMod.java
@@ -20,7 +20,7 @@ public class DocMod {
 	
     public static final String MODID = "documents";
     public static final String NAME = "Documents Mod";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.1.0";
     @Mod.Instance(MODID)
     private static DocMod INSTANCE;
 
