d48d79d3370255f2ab696b27e0a5bb3474b9010b
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-13 00:23:06 +0100

    Create DocUI.java

---
+++ neutral/src/net/fexcraft/mod/doc/ui/DocUI.java
@@ -0,0 +1,19 @@
+package net.fexcraft.mod.doc.ui;
+
+import net.fexcraft.mod.doc.Documents;
+import net.fexcraft.mod.uni.UniReg;
+import net.fexcraft.mod.uni.ui.UIKey;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class DocUI {
+
+    public static UIKey EDITOR = new UIKey(0, "documents:editor");
+    public static UIKey VIEWER = new UIKey(0, "documents:viewer");
+
+    public static void register(Documents mod){
+        UniReg.registerMod("documents", mod);
+    }
+
+}
