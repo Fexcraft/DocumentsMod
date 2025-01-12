dc5589ef2ab721add90fec2b191a37935a31db8b
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-12 23:03:59 +0100

    Update DocItemMeshDef.java

--- forge12/src/main/java/net/fexcraft/mod/doc/DocItemMeshDef.java
+++ forge12/src/main/java/net/fexcraft/mod/doc/DocItemMeshDef.java
@@ -5,14 +5,14 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
-import static net.fexcraft.mod.doc.DocRegistry.NBTKEY;
+import static net.fexcraft.mod.doc.DocRegistry.NBTKEY_TYPE;
 
 public class DocItemMeshDef implements ItemMeshDefinition {
 
 	@Override
 	public ModelResourceLocation getModelLocation(ItemStack stack){
-		if(stack.hasTagCompound() && stack.getTagCompound().hasKey(NBTKEY)){
-			return new ModelResourceLocation(new ResourceLocation(DocMod.MODID, stack.getTagCompound().getString(NBTKEY)), "inventory");
+		if(stack.hasTagCompound() && stack.getTagCompound().hasKey(NBTKEY_TYPE)){
+			return new ModelResourceLocation(new ResourceLocation(DocMod.MODID, stack.getTagCompound().getString(NBTKEY_TYPE)), "inventory");
 		}
 		return new ModelResourceLocation(DocumentItem.INSTANCE.getRegistryName(), "inventory");
 	}
