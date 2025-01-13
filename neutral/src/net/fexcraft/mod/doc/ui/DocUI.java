262f2060ce0460dea5d809ba73a1e522c03c3ff8
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-13 23:34:23 +0100

    Update DocUI.java

--- neutral/src/net/fexcraft/mod/doc/ui/DocUI.java
+++ neutral/src/net/fexcraft/mod/doc/ui/DocUI.java
@@ -10,12 +10,14 @@ import net.fexcraft.mod.uni.ui.UIKey;
 public class DocUI {
 
     public static UIKey EDITOR = new UIKey(0, "documents:editor");
-    public static UIKey VIEWER = new UIKey(0, "documents:viewer");
+    public static UIKey VIEWER = new UIKey(1, "documents:viewer");
 
     public static void register(Documents mod){
         UniReg.registerMod("documents", mod);
         UniReg.registerUI(EDITOR, DocEditorUI.class);
         UniReg.registerMenu(EDITOR, "documents:uis/editor", DocEditorCon.class);
+        UniReg.registerUI(VIEWER, DocViewerUI.class);
+        UniReg.registerMenu(VIEWER, "documents:uis/viewer", DocViewerCon.class);
     }
 
 }
