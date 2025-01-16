390c405027a88a312fa4432a1e9f005880ff70fb
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-16 21:13:58 +0100

    Update DocEventHandler.java

--- forge12/src/main/java/net/fexcraft/mod/doc/DocEventHandler.java
+++ forge12/src/main/java/net/fexcraft/mod/doc/DocEventHandler.java
@@ -1,19 +1,14 @@
 package net.fexcraft.mod.doc;
 
-import net.fexcraft.app.json.JsonHandler;
-import net.fexcraft.app.json.JsonHandler.PrintOption;
-import net.fexcraft.lib.mc.network.PacketHandler;
-import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.mod.uni.UniEntity;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
 
 public class DocEventHandler {
 	
@@ -32,11 +27,6 @@ public class DocEventHandler {
 	public void onJoin(PlayerLoggedInEvent event){
 		if(event.player.world.isRemote) return;
 		DocRegistry.onPlayerJoin(UniEntity.getEntity(event.player));
-		NBTTagCompound com = new NBTTagCompound();
-		com.setString("target_listener", "docmod");
-		com.setString("task", "sync");
-		com.setString("data", JsonHandler.toString(DocRegistry.getSyncMap(), PrintOption.FLAT));
-		PacketHandler.getInstance().sendTo(new PacketNBTTagCompound(com), (EntityPlayerMP)event.player);
 	}
 	
 	@SubscribeEvent
