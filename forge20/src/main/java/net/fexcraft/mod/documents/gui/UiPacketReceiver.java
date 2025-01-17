02730b07a7cdf844559ecf373652f8af602e9e71
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-18 00:24:40 +0100

    Delete UiPacketReceiver.java

--- forge20/src/main/java/net/fexcraft/mod/documents/gui/UiPacketReceiver.java
+++
@@ -1,9 +0,0 @@
-package net.fexcraft.mod.documents.gui;
-
-import net.minecraft.nbt.CompoundTag;
-
-public interface UiPacketReceiver {
-
-	public void onPacket(CompoundTag com, boolean client);
-
-}
