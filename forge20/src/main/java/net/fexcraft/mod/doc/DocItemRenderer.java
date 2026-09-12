fb23f8cb8bacbc1814648e9bc56f73bf3e5c4423
Ferdinand Calo <fexcalo@gmail.com>
2026-09-12 20:24:56 +0200

    Update DocItemRenderer.java

--- forge20/src/main/java/net/fexcraft/mod/doc/DocItemRenderer.java
+++ forge20/src/main/java/net/fexcraft/mod/doc/DocItemRenderer.java
@@ -2,11 +2,12 @@ package net.fexcraft.mod.doc;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fexcraft.lib.common.Static;
-import net.fexcraft.lib.tmt.ModelRendererTurbo;
+import net.fexcraft.lib.frl.Polyhedron;
+import net.fexcraft.lib.frl.gen.Generator;
 import net.fexcraft.mod.doc.data.DocStackApp;
 import net.fexcraft.mod.doc.data.Document;
 import net.fexcraft.mod.fcl.util.FCLRenderTypes;
-import net.fexcraft.mod.fcl.util.Renderer120;
+import net.fexcraft.mod.fcl.util.Renderer20;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.inv.UniStack;
 import net.minecraft.client.Minecraft;
@@ -17,14 +18,17 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.util.NonNullLazy;
 import org.joml.Quaternionf;
 
-import static net.fexcraft.mod.fcl.util.Renderer120.*;
+import static net.fexcraft.lib.frl.gen.Generator.Values.WIDTH;
+import static net.fexcraft.lib.frl.gen.Generator.Values.HEIGHT;
+import static net.fexcraft.lib.frl.gen.Generator.Values.DEPTH;
+import static net.fexcraft.mod.fcl.util.Renderer20.AX;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class DocItemRenderer {
 
-	private static ModelRendererTurbo model = new ModelRendererTurbo(null, 0, 0, 1, 1).newBoxBuilder().setSize(1, 1, 0).build();
+	private static Polyhedron poly = new Generator(Generator.Type.CUBOID).set(WIDTH, 1f).set(HEIGHT, 1f).set(DEPTH, 0f).make().genNorm();
 	private static Document doc;
 	private static IDL texture;
 
@@ -37,12 +41,12 @@ public class DocItemRenderer {
 				doc = app.getDocument();
 				if(doc != null) texture = doc.itemicon;
 			}
-			Renderer120.set(pose, src, v0);
+			Renderer20.set(pose, src, v0);
 			FCLRenderTypes.setCutout(texture);
 			pose.pushPose();
 			pose.translate(0, 1, 0.5);
 			pose.mulPose(new Quaternionf().rotateAxis(Static.rad180, AX));
-			model.render(1f);
+			poly.render();
 			pose.popPose();
 		}
 	});
